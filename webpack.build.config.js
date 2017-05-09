673eb39de91c14c0ab6536a1b07e05aa0b9fb803
Matt Sanders <matt.d.s.sanders@gmail.com>
2017-05-10 09:08:28 +1200

    updated webpack to remove bloat and build 2.2.3

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -26,12 +26,17 @@ module.exports = [
         },
 
         plugins: [
-            new webpack.optimize.DedupePlugin(),
             new webpack.DefinePlugin({
                 'process.env' : {
                     NODE_ENV : JSON.stringify('production')
                 }
             }),
+	  new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+	  }),
+	  new webpack.optimize.DedupePlugin(),
             new webpack.BannerPlugin(banner, {
                 raw: true
             }),
